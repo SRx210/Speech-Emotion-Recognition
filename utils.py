1abb6ff3d91ff0a8cdc33f89124d433a450aee12
Sohan Raut <sohandr9090@gmail.com>
2025-07-05 09:56:31 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-import librosa
-import numpy as np
-
-def extract_features(file_path):
-    audio, sample_rate = librosa.load(file_path)
-    mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
-    mfccs_scaled = np.mean(mfccs.T, axis=0)
-    return mfccs_scaled
